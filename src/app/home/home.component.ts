4f6cb3832bb5dd7e01e979219a39b0fd903b9ad2
Alexander Honer <ahoner@umd.edu>
2017-07-18 00:04:59 -0400

    Finished card content animations (finally).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import {
   transition,
   animate,
   keyframes,
+  state,
   query,
   stagger
 } from '@angular/animations';
@@ -15,7 +16,7 @@ import {
   styleUrls: ['./home.component.css'],
   animations: [
     trigger('listAnimation',[
-      transition('* => *', [
+      transition(':enter', [
         query('md-card', style({
           opacity: 0,
           transform: 'translateY(50px)'
@@ -29,31 +30,89 @@ import {
           ])
         ], { optional: true }),
       ]),
+    ]),
+    trigger('drawerAnimation', [
+      state('closed', style({
+        height: '0'
+      })),
+      state('open', style({
+        height: '200px'
+      })),
+      transition('closed <=> open',
+        animate('500ms ease'))
+    ]),
+    trigger('drawerContainerAnimation', [
+      state('in', style({transform: 'translateX(0)', opacity: 1})),
+      transition('void => in', [
+        style({transform: 'translateX(-50%)', opacity: 0}),
+        animate('500ms ease-out')
+      ]),
+      transition('in => void', [
+        animate('500ms ease-in', style({transform: 'translateX(50%)', opacity: 0}))
+      ])
     ])
   ]
 })
 export class HomeComponent {
-  content = 'Music stuff.';
-  contentCards = [];
+  contentCards = [{
+    id: '1',
+    title: 'What is the program?',
+    image: 'assets/BigDrumBoisWithTone.jpg',
+    state: 'in',
+    content: 'The Young Musicians Summer Academy is a unique summer camp designed to offer musical enrichment to middle and high school students through musical workshops, lessons, and performance opportunities with guest artist faculty. '
+  }, {
+    id: '2',
+    title: 'When is the program?',
+    image: 'assets/LowStringyBoi.jpg',
+    state: 'in',
+    content: `June 27-29, 2017@ North Caroline High School
+Download the Application or Click Here to Register Online
+Especially designed for standard band & orchestra instrumentalists, incoming 6th–12th grades.
+Final performance will be at 3:30 on Thursday in the auditorium.
+The public is welcome!`
+  }, {
+    id: '3',
+    title: 'Moo',
+    image: 'assets/BigDrumBoisWithTone.jpg',
+    state: 'in',
+    content: 'filler'
+  }, {
+    id: '4',
+    title: 'Moo',
+    image: 'assets/BigDrumBoisWithTone.jpg',
+    state: 'in',
+    content: 'filler'
+  }, {
+    id: '5',
+    title: 'Moo',
+    image: 'assets/BigDrumBoisWithTone.jpg',
+    state: 'in',
+    content: 'filler'
+  }];
+  drawerState = 'closed';
+  selectedCards = [];
+  nextCard = {};
+
+  drawerContainerAnimationDone($event) {
+    if($event.toState === 'void') {
+      this.selectedCards.push(this.nextCard);
+      this.nextCard = {};
+    }
+  }
+
+  toggleDrawer(card) {
+    if(this.selectedCards.length === 0) {
+      this.selectedCards.push(card);
+      this.drawerState = 'open';
+    } else if(this.selectedCards[0].id === card.id) {
+      this.selectedCards.pop();
+      this.drawerState = 'closed';
+    } else {
+      this.selectedCards.pop();
+      this.nextCard = card;
+    }
+  }
 
   constructor() {
-    this.contentCards = [
-      {
-        title: 'What is the program?',
-        image: 'assets/BigDrumBoisWithTone.jpg'
-      }, {
-        title: 'When is the program?',
-        image: 'assets/LowStringyBoi.jpg'
-      }, {
-        title: 'Moo',
-        image: 'assets/BigDrumBoisWithTone.jpg'
-      }, {
-        title: 'Moo',
-        image: 'assets/BigDrumBoisWithTone.jpg'
-      }, {
-        title: 'Moo',
-        image: 'assets/BigDrumBoisWithTone.jpg'
-      }
-    ]
   }
 }
